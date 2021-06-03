4d9252facbc9ddbd2433302bbce1c984f19a832f
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-06-03 10:56:50 -0500

    built out the controllers

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -1,2 +1,28 @@
 class CreaturesController < ApplicationController
+
+    def index
+        creatures = Creature.all
+        render json:CreatureSerializer.new(creatures)
+    end
+
+    def create
+        creature = Creature.new
+        if creature.save
+            render json: CreatureSerializer.new(creature)
+        else
+            render json: {errors: creature.errors.full_messages}
+        end
+    end
+
+    def destroy
+        creature = Creature.find(params[:id])
+        creature.destroy
+    end
+
+    private
+
+    def creature_params
+        params.require(:creature).permot(:name, :image, :description)
+    end
+
 end
--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,2 +1,27 @@
 class SkillsController < ApplicationController
+
+    def index
+        skills = Skill.all
+        render json: skills
+    end
+
+    def create
+        skill = Skill.new(skill_params)
+        if skill.new
+            render json: skill
+        else
+            render json: {errors: skill.errors.full_messages}
+        end
+    end
+
+    def destroy
+        skill = Skill.find(params[:id])
+        skill.destroy
+    end
+
+    private
+
+    def skil_params
+        params.require(:skill).permit(:name, :creature_id)
+    end
 end
